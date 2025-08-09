27d166ee8c7b1797ef4d434b02d3de0c45f623ab
Syeda Alishba <alishbasyeda057@gmail.com>
2025-08-09 16:20:47 +0500

    panda

--- src/Geometry/Canvas.java
+++ src/Geometry/Canvas.java
@@ -15,7 +15,13 @@ public class Canvas extends JPanel {
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         g.setColor(Color.BLUE);
-       g.drawOval(150,150,200,200);// only outline of a circle
-       g.fillOval(10,10,100,100); //we can create an circle with this only but this creates a filled circle
+//      g.drawOval(150,150,200,200);// only outline of a circle
+        g.fillOval(100, 80, 80, 80); //we can create a circle with this only but this creates a filled circle
+        g.fillOval(320, 80, 80, 80);
+        g.fillOval(150, 100, 200, 200);
+        g.setColor(Color.BLACK);
+        g.fillOval(200, 150, 20, 20);
+       g.fillOval(270, 150, 20, 20);
+
     }
 }
